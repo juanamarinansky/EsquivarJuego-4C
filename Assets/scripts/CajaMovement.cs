210e041f9107e62d3ccc5e9c70900515bf28e425
juanamarinansky <juanamarinansky1@gmail.com>
2026-05-07 12:00:11 -0300

    random position

--- Assets/scripts/CajaMovement.cs
+++ Assets/scripts/CajaMovement.cs
@@ -22,10 +22,21 @@ public class CajaMovement : MonoBehaviour
     
     public void MoveBoxToRandomPosition()
     {
-        // para probar, solamente hacemos que suba
-        //No vale hacer transform.position.y = initialYValue. 
         Vector3 newPosition = transform.position;
+        int randomX = 0;
+        randomX = Random.Range(0,3);
+        if(randomX == 0){
+            newPosition.x = -2;
+        }
+        else if(randomX == 1){
+            newPosition.x = 0;
+        }
+        else{
+            newPosition.x = 2;
+        }
+
         newPosition.y = initialYValue;
         transform.position = newPosition;
+
     }
 }
